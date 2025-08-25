62a71faf1b627a84d9575682600fc5be1346736f
prashantgawhade-oss <prashantgawhade@gmail.com>
2025-08-25 15:36:08 +0530

    Create Day01_Prashant.js

---
+++ DAY_1/Day01_Prashant.js
@@ -0,0 +1,8 @@
+
+let Studentdetails={
+  Name:"Martin",Age:17,
+  Course:"B.tech",
+  Branch:"Electrical and Electronics Engineering",
+  EnrollmentNumber:"0502EX1024",
+}
+console.log("Studentdetails",Studentdetails);
