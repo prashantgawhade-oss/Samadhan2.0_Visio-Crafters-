8b66b0d2b78e7ad1747250981a207f2efb8e5435
Md Gulzar Akhtar <akhterg303@gmail.com>
2025-08-25 15:51:30 +0530

    Create gulzar1.js

---
+++ Day01/gulzar1.js
@@ -0,0 +1,9 @@
+const Studentdetails = {
+  Name: "Ayesha Khan",
+  Age: 19,
+  Course: "B.Sc",
+  Branch: "Computer Science",
+  EnrollmentNumber: "0703CS2045",
+};
+
+console.log("Student Details:", Studentdetails);
