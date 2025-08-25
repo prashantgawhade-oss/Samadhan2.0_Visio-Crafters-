f061deb565903cb86e5e8286e11948dc799f1ee8
prashantgawhade-oss <prashantgawhade@gmail.com>
2025-08-25 15:39:24 +0530

    Delete Day02_Prashant.js

--- Day02_Prashant.js
+++
@@ -1,9 +0,0 @@
-//This is for finding highest marks from an array(loop)
-let Marks=[54,60,79,38,60]
-let highest= Marks[0];
-for(let i=1;i<Marks.length;i++){
-  if(Marks[i]>highest){
-    highest=Marks[i];
-  }
-}
-console.log("HighestMarks",highest);
