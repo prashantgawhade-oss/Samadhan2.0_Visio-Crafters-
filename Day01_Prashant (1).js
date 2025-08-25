564273365d5c08b1c77929c016e133c3b89da79f
prashantgawhade-oss <prashantgawhade@gmail.com>
2025-08-25 15:38:55 +0530

    Delete Day01_Prashant (1).js

--- Day01_Prashant (1).js
+++
@@ -1,8 +0,0 @@
-// This code is for day 01 task 
-let Studentdetails={
-  Name:"Martin",Age:17,
-  Course:"B.tech",
-  Branch:"Electrical and Electronics Engineering",
-  EnrollmentNumber:"0502EX1024",
-}
-console.log("Studentdetails",Studentdetails);
