9af928ffc529ea305d0317baeab2990eeae92053
Md Gulzar Akhtar <akhterg303@gmail.com>
2025-08-26 12:30:55 +0530

    Create Gulzar.js

---
+++ Day04/Gulzar.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = 5000;
+
+app.get("/", function (req, res) {
+  res.send("Welcome to Samadhan 2.0");
+});
+
+app.get("/api/hello", function (req, res) {
+  res.send("Hello, World!");
+});
+
+app.listen(PORT, function () {
+  console.log("✅ Server running on http://localhost:" + PORT);
+});
